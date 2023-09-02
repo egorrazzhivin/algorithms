9aaec1d66cceb7d9410583275de012daf9fd85af
Egor Razzhivin <110990740+egorrazzhivin@users.noreply.github.com>
2023-09-02 23:13:19 +0300

    Create string_operations.cpp

---
+++ data structures/string_operations.cpp
@@ -0,0 +1,80 @@
+bool isSmaller(string str1, string str2){
+    int n1 = str1.length(), n2 = str2.length();
+    if (n1 < n2)
+        return true;
+    if (n2 < n1)
+        return false;
+    for (int i = 0; i < n1; i++)
+        if (str1[i] < str2[i])
+            return true;
+        else if (str1[i] > str2[i])
+            return false;
+    return false;
+}
+ 
+
+string add(string str1, string str2){
+    if (str1.length() > str2.length())
+        swap(str1, str2);
+    string str = "";
+    int n1 = str1.length(), n2 = str2.length();
+    reverse(str1.begin(), str1.end());
+    reverse(str2.begin(), str2.end());
+ 
+    int carry = 0;
+    for (int i=0; i<n1; i++){
+        int sum = ((str1[i]-'0')+(str2[i]-'0')+carry);
+        str.push_back(sum%10 + '0');
+        carry = sum/10;
+    }
+    for (int i=n1; i<n2; i++){
+        int sum = ((str2[i]-'0')+carry);
+        str.push_back(sum%10 + '0');
+        carry = sum/10;
+    }
+    if (carry) str.push_back(carry+'0');
+  
+    reverse(str.begin(), str.end());
+    return str;
+}
+
+string subtract(string str1, string str2)
+{
+    if (isSmaller(str1, str2))
+        swap(str1, str2);
+    string str = "";
+    int n1 = str1.length(), n2 = str2.length();
+    reverse(str1.begin(), str1.end());
+    reverse(str2.begin(), str2.end());
+    int carry = 0;
+ 
+    for (int i = 0; i < n2; i++) {
+        int sub = ((str1[i] - '0') - (str2[i] - '0') - carry);
+        if (sub < 0) {
+            sub = sub + 10;
+            carry = 1;
+        }
+        else carry = 0;
+        str.push_back(sub + '0');
+    }
+    for (int i = n2; i < n1; i++) {
+        int sub = ((str1[i] - '0') - carry);
+        if (sub < 0) {
+            sub = sub + 10;
+            carry = 1;
+        }
+        else carry = 0;
+        str.push_back(sub + '0');
+    }
+    reverse(str.begin(), str.end());
+    int i = 0;
+    while(str[i]=='0') i++;
+    string res;
+    while(i!=(int)str.length()){
+        res += str[i];
+        i++;
+    }
+    return res;
+}
+
+
