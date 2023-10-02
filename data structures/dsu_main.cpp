4df92d402b56dd846a6d4211767159fa92528923
Egor Razzhivin <110990740+egorrazzhivin@users.noreply.github.com>
2023-10-03 01:04:46 +0300

    Update dsu_main.cpp

--- data structures/dsu_main.cpp
+++ data structures/dsu_main.cpp
@@ -2,15 +2,15 @@ struct dsu {
     int n;
     vector<int> p, sz;
     dsu(int n){
-        p.resize(n+1);
-        sz.resize(n+1);
-        for(int i =0;i<=n;i++) {
-            p[i]=i; 
+        p.resize(n + 1);
+        sz.resize(n + 1);
+        for(int i = 0; i <= n; i++) {
+            p[i] = i; 
             sz[i] = 1;
         }
     }
     int find(int a){ 
-        return a==p[a] ? a : p[a] = find(p[a]); 
+        return a == p[a] ? a : p[a] = find(p[a]); 
     }
     int get_size(int a){
         return sz[a];
@@ -18,7 +18,7 @@ struct dsu {
     void merge(int a, int b){
         a = find(a);
         b = find(b);
-        if(a!=b) {
+        if(a != b) {
             if(sz[a] < sz[b]) swap(a,b);
             p[b] = a;
             sz[a] += sz[b];
