29bc142ff9a8fbfc710274e75c36a3faf376a290
Egor Razzhivin <110990740+egorrazzhivin@users.noreply.github.com>
2023-11-06 01:12:00 +0300

    Create decision_tree_split.py

---
+++ ML/decision_tree_split.py
@@ -0,0 +1,42 @@
+import numpy as np
+import typing as tp
+def create_split(feature_values: np.ndarray, threshold: float) -> tp.Tuple[np.ndarray, np.ndarray]:
+    """
+    splits given 1-d array according to relation to threshold into two subarrays
+    :param feature_values: feature values extracted from data
+    :param threshold: value to compare with
+    :return: two sets of indices
+    """
+    left_idx = np.where(feature_values <= threshold)[0]
+    right_idx = np.where(feature_values > threshold)[0]
+
+    return left_idx, right_idx
+
+
+def _best_split(self, X: np.ndarray, y: np.ndarray):
+    """
+    finds best split
+    :param X: Data, passed to node
+    :param y: labels
+    :return: best feature, best threshold, left child impurity, right child impurity
+    """
+    lowest_impurity = np.inf
+    best_feature_id = None
+    best_threshold = None
+    lowest_left_child_impurity, lowest_right_child_impurity = None, None
+    features = self._meta.rng.permutation(X.shape[1])
+    for feature in features:
+        current_feature_values = X[:, feature]
+        thresholds = np.unique(current_feature_values)
+        for threshold in thresholds:
+            left_idx, right_idx = create_split(current_feature_values, threshold)
+            y_left, y_right = y[left_idx], y[right_idx]
+            current_weighted_impurity, current_left_impurity, current_right_impurity = weighted_impurity(y_left, y_right)
+            if current_weighted_impurity < lowest_impurity:
+                lowest_impurity = current_weighted_impurity
+                best_feature_id = feature
+                best_threshold = threshold
+                lowest_left_child_impurity = current_left_impurity
+                lowest_right_child_impurity = current_right_impurity
+
+    return best_feature_id, best_threshold, lowest_left_child_impurity, lowest_right_child_impurity
