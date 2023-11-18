dc32af807aabfcbef323d6af9e549b1a92c098fe
Egor Razzhivin <110990740+egorrazzhivin@users.noreply.github.com>
2023-11-18 22:23:53 +0300

    Update segtree_sum.cpp

--- data structures/segtree_sum.cpp
+++ data structures/segtree_sum.cpp
@@ -1,55 +1,38 @@
-struct segtree{
-    int n,sz;
-    vector<ll> tree;
+struct segtree {
+    int n;
+    vector<int64_t> t;
 
-    segtree(int n){
-        sz=1;
-        while(sz<n)sz*=2;
-        tree.assign(2*sz-1, 0);
+    segtree(int n) {
+        t.assign(4 * n + 5, 0);
     }
 
-    void init(vector<int>& a, int x,int lx, int rx){
-        if(rx-lx==1) {
-            if(lx<a.size()) tree[x]=a[lx];
-        }
-        else{
-            int m = (lx+rx)>>1;
-            init(a,2*x+1,lx,m);
-            init(a,2*x+2,m,rx);
-            tree[x] = tree[2*x+1]+tree[2*x+2];
-        }
-    }
-
-    void build(vector<int>& a){
-        init(a,0,0,sz);
-    }
-
-    void update(int i, int v, int x, int lx, int rx){
-        if(rx-lx==1) {
-            tree[x] = v;
+    void build(int v, int l, int r, vector<int>& a) {
+        if (l == r) {
+            t[v] = a[l];
             return;
         }
-        int m = (lx+rx)>>1;
-        if(i<m) update(i, v, 2*x+1, lx,m);
-        else update(i,v,2*x+2,m,rx);
-        tree[x] = tree[2*x+1]+tree[2*x+2];
+        int m = (l + r) >> 1;
+        // [l, m], [m + 1, r];
+        build(v << 1, l, m, a);
+        build(v << 1 | 1, m + 1, r, a);
+        t[v] = t[v << 1] + t[v << 1 | 1];
     }
 
-    void set(int i, int v){
-        update(i,v,0,0,sz);
+    void update(int v, int l_seg, int r_seg, int index, int x) {
+        if (l_seg == r_seg) {
+            t[v] = x;
+            return;
+        }
+        int m = (l_seg + r_seg) >> 1;
+        if (index <= m) update(v << 1, l_seg, m, index, x);
+        else update(v << 1 | 1, m + 1, r_seg, index, x);
+        t[v] = t[v << 1] + t[v << 1 | 1];
     }
 
-    ll get_sum(int l, int r, int x, int lx, int rx){
-        if(rx<=l || r<= lx) return 0;
-        if(lx>=l && rx<=r) return tree[x];
-        int m = (lx+rx)>>1;
-        ll s1 = get_sum(l,r,2*x+1,lx,m);
-        ll s2 = get_sum(l,r,2*x+2,m,rx);
-        return s1 + s2;
+    int64_t get(int v, int l_seg, int r_seg, int l, int r) {
+        if (l_seg > r || r_seg < l) return 0;
+        if (l_seg >= l && r_seg <= r) return t[v];
+        int m = (l_seg + r_seg) >> 1;
+        return get(v << 1, l_seg, m, l, r) + get(v << 1 | 1, m + 1, r_seg, l, r);
     }
-
-    ll sum(int l, int r){
-        return get_sum(l,r,0,0,sz);
-    }
-
 };
