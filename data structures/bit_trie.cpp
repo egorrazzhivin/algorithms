d8deb075b1b3920d36d132ebaae0a957bc723751
Egor Razzhivin <110990740+egorrazzhivin@users.noreply.github.com>
2024-01-11 23:52:37 +0700

    Update bit_trie.cpp

--- data structures/bit_trie.cpp
+++ data structures/bit_trie.cpp
@@ -1,7 +1,7 @@
 const int LOG = 30;
 
-struct bit_trie{
-    struct node{
+struct bit_trie {
+    struct node {
         int cnt, right, left;
         node() {
             cnt = 0;
@@ -10,25 +10,25 @@ struct bit_trie{
     };
     int n, new_vertex;
     vector<node> trie;
-    bit_trie(int n){
+    bit_trie (int n) {
         trie.resize(n);
         new_vertex = 1;
     }
-    void insert(int x){
-        int v = 0; //current vertex
-        for(int bit = LOG - 1; bit>=0; bit--){
-            if(x&(1<<bit)){
-                //right child
-                if(trie[v].right == -1){
+    void insert(int x) {
+        int v = 0; // current vertex
+        for (int bit = LOG - 1; bit >= 0; bit--) {
+            if (x & (1 << bit)) {
+                // right child
+                if (trie[v].right == -1) {
                     trie[v].right = new_vertex;
                     new_vertex++;
                 }
                 trie[trie[v].right].cnt++;
                 v = trie[v].right;
             }
-            else{
-                //left child
-                if(trie[v].left == -1){
+            else {
+                // left child
+                if (trie[v].left == -1) {
                     trie[v].left = new_vertex;
                     new_vertex++;
                 }
