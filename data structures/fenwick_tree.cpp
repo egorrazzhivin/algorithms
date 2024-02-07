8241dbd721c6e7528433f5a5164d052e5636c42f
Egor Razzhivin <110990740+egorrazzhivin@users.noreply.github.com>
2024-02-08 01:36:57 +0300

    Update fenwick_tree.cpp

--- data structures/fenwick_tree.cpp
+++ data structures/fenwick_tree.cpp
@@ -6,14 +6,14 @@ struct fenwick{
     }
 
     void inc(int pos, int64_t val) {
-        for (int i = pos; i < static_cast<int>(fw.size()); i = (i | (i+1))) {
+        for (int i = pos; i < static_cast<int>(fw.size()); i = (i | (i + 1))) {
             fw[i] += val;
         }
     }
 
     int64_t get(int right) {
         int64_t res = 0;
-        for (int i = right; i >= 0; i = (i & (i+1)) - 1) {
+        for (int i = right; i >= 0; i = (i & (i + 1)) - 1) {
             res += fw[i];
         }
         return res;
