9e49f66cf2938990ec04d1e7ab59b58804c42810
Egor Razzhivin <110990740+egorrazzhivin@users.noreply.github.com>
2024-02-07 20:15:24 +0300

    Update randomized_stuff.cpp

--- data structures/randomized_stuff.cpp
+++ data structures/randomized_stuff.cpp
@@ -3,9 +3,6 @@ cout << uniform_int_distribution<int>(l,r)(rng) << endl;
 
 -----------
 
-namespace generator {
-    mt19937 generator(132132);
-}
-
-uniform_int_distribution<int64_t> prior(0, 1e9);
-int64_t value = prior(generator::generator);
+mt19937 generator(chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int64_t> prior(0, 1e15);
+int64_t value = prior(generator);
