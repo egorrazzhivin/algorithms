0df0285c59132f2bb5d56e7b0455a952dc016723
Egor Razzhivin <110990740+egorrazzhivin@users.noreply.github.com>
2024-03-25 19:22:13 +0300

    Create manacher.cpp

---
+++ data structures/manacher.cpp
@@ -0,0 +1,36 @@
+// used here https://codeforces.com/contest/1943/problem/B
+
+vector<int> manacher_odd(string s) {
+    int n = s.size();
+    s = "$" + s + "^";
+    vector<int> p(n + 2);
+    int l = 1, r = 1;
+    for(int i = 1; i <= n; i++) {
+        p[i] = max(0, min(r - i, p[l + (r - i)]));
+        while(s[i - p[i]] == s[i + p[i]]) {
+            p[i]++;
+        }
+        if(i + p[i] > r) {
+            l = i - p[i], r = i + p[i];
+        }
+    }
+    return vector<int>(begin(p) + 1, end(p) - 1);
+}
+
+vector<int> manacher(string s) {
+    string t;
+    for(auto c: s) {
+        t += string("#") + c;
+    }
+    t += "#";
+    auto res = manacher_odd(t);
+    return vector<int>(begin(res) + 1, end(res) - 1);
+}
+
+// 0-indexed ?
+
+int main() {
+    string s;
+    cin >> s;
+    vector<int> man = manacher(s);
+}
