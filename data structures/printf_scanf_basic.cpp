bfb0edc97bac2461511670083627f1cf1525946b
Egor Razzhivin <110990740+egorrazzhivin@users.noreply.github.com>
2024-06-24 01:29:38 +0300

    Create printf_scanf_basic.cpp

---
+++ data structures/printf_scanf_basic.cpp
@@ -0,0 +1,26 @@
+// please be patient i have autism
+
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+    int x;
+    scanf("%d", &x);
+    printf("%d\n", x);
+    double y;
+    scanf("%lf", &y);
+    printf("lf\n", y);
+    int a, b;
+    double c;
+    scanf("%d %d %lf", &a, &b, &c);
+    printf("%d %d %lf\n", a, b, c);
+    /*
+    2
+    2
+    2.312412
+    2.31
+    5 2 4.3123
+    5 2 4.31
+    */
+}
