4e59c1ec152ab51ba3391106992c81217b448514
Egor Razzhivin <110990740+egorrazzhivin@users.noreply.github.com>
2024-07-11 02:01:31 +0300

    Create segtree_sum_descent.cpp

---
+++ data structures/segtree_sum_descent.cpp
@@ -0,0 +1,54 @@
+struct segtree {
+    int n;
+    vector<int64_t> t;
+
+    segtree(int n) {
+        t.assign(4 * n + 5, 0);
+    }
+
+    void build(int v, int l, int r, vector<int>& a) {
+        if (l == r) {
+            t[v] = a[l];
+            return;
+        }
+        int m = (l + r) >> 1;
+        build(v << 1, l, m, a);
+        build(v << 1 | 1, m + 1, r, a);
+        t[v] = t[v << 1] + t[v << 1 | 1];
+    }
+
+    void update(int v, int l_seg, int r_seg, int index, int x) {
+        if (l_seg == r_seg) {
+            t[v] = x;
+            return;
+        }
+        int m = (l_seg + r_seg) >> 1;
+        if (index <= m) update(v << 1, l_seg, m, index, x);
+        else update(v << 1 | 1, m + 1, r_seg, index, x);
+        t[v] = t[v << 1] + t[v << 1 | 1];
+    }
+
+    int64_t get(int v, int l_seg, int r_seg, int l, int r) {
+        if (l_seg > r || r_seg < l) return 0;
+        if (l_seg >= l && r_seg <= r) return t[v];
+        int m = (l_seg + r_seg) >> 1;
+        return get(v << 1, l_seg, m, l, r) + get(v << 1 | 1, m + 1, r_seg, l, r);
+    }
+
+    int find(int v, int l_seg, int r_seg, int l, int k) {
+        // find k'th 1 on segment [l, n]
+        if (l_seg == r_seg) {
+            // found
+            return l_seg;
+        }
+        if (l > 1) k += get(1, 1, n, 1, l - 1); // shift so find k'th 1 on [1, n]
+        // I DIDN'T CHECK l > 1 CASE BUT HOPE IT'LL WORK
+        int m = (l_seg + r_seg) >> 1;
+        int l_value = t[v << 1], r_value = t[v << 1 | 1];
+        if (l_value >= k) return find(v << 1, l_seg, m, 1, k);
+        else return find(v << 1 | 1, m + 1, r_seg, 1, k - l_value);
+    }
+};
+
+// st.build(1, 1, n, a); a - 1-indexed
+// st.get(1, 1, n, l, r)
